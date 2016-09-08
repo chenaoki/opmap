c56c9f585aa5245529ed83e05ffab310e92f6bd5
tomii <naoki_tomii@bmpe.t.u-tokyo.ac.jp>
2016-09-08 22:38:01 +0900

    experiment.pyにintervalのoptionを追加

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -1,22 +1,28 @@
 import sys
 import os
-from opmap.opmap import VmemMap
+from opmap.opmap import VmemMap, RawCam
 import json
 
-if len(sys.argv) != 2:
-    print('Please enter diredtory name')
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+    print('Please check argument')
     exit()
 
 with open("experiment.json", "r") as f : params = json.load(f)
 param_vmem = params["basic"]
 diff_min = params["option"]["diff_min"]
 smooth_size = params["option"]["smooth_size"]
+interval_time = params["option"]["interval"]
 
-dir_name = sys.argv[1]
+if len(sys.argv) == 2:
+	dir_name = sys.argv[1]
+else:
+	dir_name = sys.argv[1]
+	interval_time = int(sys.argv[2])
 param_vmem["path"] = dir_name
 
-vmem = VmemMap(**param_vmem)
+cam = RawCam(**param_vmem)
+vmem = VmemMap(cam)
 vmem.setDiffRange(diff_min=diff_min)
 if smooth_size > 0 : vmem.smooth(size=smooth_size)
 
-vmem.saveMovie(dir_name+"/vmem.mp4")
+vmem.saveMovie(dir_name+"/vmem.mp4", interval=interval_time)
