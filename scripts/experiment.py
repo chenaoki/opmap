aaec2e2e890ccbc2f84d8ec4f380e7c204d3fc01
tomii@kanken <chenaoki@gmail.com>
2016-09-09 17:27:15 +0900

    experiment.py更新

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -3,26 +3,23 @@ import os
 from opmap.opmap import VmemMap, RawCam
 import json
 
-if len(sys.argv) < 2 or len(sys.argv) > 3:
-    print('Please check argument')
-    exit()
-
 with open("experiment.json", "r") as f : params = json.load(f)
 param_vmem = params["basic"]
 diff_min = params["option"]["diff_min"]
 smooth_size = params["option"]["smooth_size"]
 interval_time = params["option"]["interval"]
+flg_plot = True if params["option"]["plot"] > 0 else False
+saveDir = param_vmem["path"]+"/result/"
 
-if len(sys.argv) == 2:
-	dir_name = sys.argv[1]
-else:
-	dir_name = sys.argv[1]
-	interval_time = int(sys.argv[2])
-param_vmem["path"] = dir_name
+if not os.path.exists(saveDir):
+	os.makedirs(saveDir)
 
 cam = RawCam(**param_vmem)
 vmem = VmemMap(cam)
 vmem.setDiffRange(diff_min=diff_min)
 if smooth_size > 0 : vmem.smooth(size=smooth_size)
+vmem.saveMovie(saveDir + "vmem.mp4", interval=interval_time)
+if flg_plot:
+	points = cam.selectPoints(saveDir+"selectPoints.png")
+	vmem.plot(points, savepath = saveDir + "plot.png")
 
-vmem.saveMovie(dir_name+"/vmem.mp4", interval=interval_time)
