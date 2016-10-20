bcc813acd20dc950a8306047c9c9eb2ba8fcbb09
tomii@kanken <chenaoki@gmail.com>
2016-10-20 19:00:11 +0900

    makeMovie windows環境のデバッグ

--- opmap/opmap.py
+++ opmap/opmap.py
@@ -20,7 +20,10 @@ cam_dtype={
 }
 
 def makeMovie(path, img_type='png'):
-      os.system("ffmpeg -r 15 -y -i {0}/%06d.{1} -c:v libx264 -pix_fmt yuv420p -qscale 0 {0}.mp4".format(path, img_type))
+
+    cmd = 'ffmpeg -r 15 -y -i "{0}/%06d.{1}" -c:v libx264 -pix_fmt yuv420p -qscale 0 "{0}.mp4"'.format(path, img_type)
+    print cmd
+    os.system(cmd)
 
 
 class VideoData(object):
